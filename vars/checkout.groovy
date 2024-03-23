0f6f8ebc2928bff905981d9b0174f9e5eeb69202
trnayak11 <42715486+trnayak11@users.noreply.github.com>
2024-03-23 18:44:26 +0530

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,3 +1,3 @@
 def call(repo){    
-    steps.checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'trnayak11', url: repo]])  
+    steps.checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'gopa1990', url: repo]])  
 }
